ba473992aaa0ee7487d943cdd432a218737b5fb3
Léon Sautour <leon@sautour.net>
2022-11-03 14:48:58 +0100

    [Feature] WIP tar ball

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
+use std::fs;
 
+// ─── Arg Struct For Clap ─────────────────────────────────────────────────────────────────────────
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -7,8 +9,45 @@ struct Args {
    filename: String,
 }
 
+// ─── Payload Stuff ───────────────────────────────────────────────────────────────────────────────
+enum PayloadType {
+    File(String),
+    Folder(String)
+}
+
+fn get_payload_type(filepath: String) -> Option<PayloadType> {
+    let attr = fs::metadata(&filepath);
+
+    match attr {
+        Ok(attr) => {
+            if attr.is_dir() {
+                Some(PayloadType::Folder(filepath))
+            } else if attr.is_file() {
+                Some(PayloadType::File(filepath))
+            } else {
+                None
+            }
+        },
+        Err(_) => None
+    }
+}
+
+fn create_tar_ball(source: fs::File)
+
+fn create_copy(t: PayloadType) {
+    // Create temp dir
+    fs::create_dir("transfer_temp").expect("Directory creations failed.");
+
+    match t {
+        PayloadType::File(path) => fs::copy(path, "transfer_temp").expect("File copy failed."),
+        PayloadType::Folder(path) => {
+            let 
+        }
+    };
+
+}
+
 fn main() {
     let args = Args::parse();
-
-    println!("Hello, world!");
+    let t = get_payload_type(args.filename);
 }
